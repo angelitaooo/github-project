ea0ccf86cd4105ab195096e758363cb163535964
angela ordonez <angelita.cdp@gmail.com>
2021-07-11 14:27:03 -0500

    use localStorage WIP

--- src/components/GistDetail/GistDetail.jsx
+++ src/components/GistDetail/GistDetail.jsx
@@ -19,9 +19,25 @@ const GistDetail = () => {
   }
 
   const files = isEmpty(gistDetail) ? null : Object.values(gistDetail?.files);
-  const saveFavorite = () => {
-    console.log('saved')
+
+  const setMemoryForm = (data) => {
+
+    const getLocalStorage = JSON.parse(localStorage.getItem('githubProject'));
+    // if (getLocalStorage[data]) {
+    //   console.log('delete key')
+    // }
+    
+    const parseData =  {[data]: true};
+    console.log(parseData);
+    // const result = {[parseData]: true}
+    localStorage.setItem('githubProject', JSON.stringify(parseData));
   }
+  const saveFavorite = (file) => {
+    console.log('fiol', file);
+    setMemoryForm(file.raw_url);
+    console.log(localStorage)
+  };
+
 
   console.log(gistDetail);
   return (
@@ -42,21 +58,23 @@ const GistDetail = () => {
             <div>
               <div>
                 <span>{file?.filename}</span>
-                <button className="btn btn-green" onClick={saveFavorite}>Add to favorites</button>
-                {/* <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                />
-              </svg> */}
+                {/* <button className="py-1 px-2 bg-green-500 text-white font-semibold rounded-lg shadow-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75" onClick={saveFavorite}>Add to favorites</button> */}
+                <button onClick={(e) => saveFavorite(file)}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                    />
+                  </svg>
+                </button>
               </div>
 
               <pre className="content-wrapper">
